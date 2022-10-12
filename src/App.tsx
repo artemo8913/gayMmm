d77271f892acbfa35a168e8f60c7592a6e9ae674
LAPTOP-L9O08U7Q\kalin <kaline-son@ya.ru>
2022-10-12 22:12:33 +0700

    add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { LoginPage } from "./LoginPage";
 import { GaymPage } from "./GaymPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 export function App() {
   return (
-    <div>
-      <LoginPage />
-      <GaymPage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />}></Route>
+        <Route path="/game" element={<GaymPage />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
