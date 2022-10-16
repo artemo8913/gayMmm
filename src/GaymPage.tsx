7be21912c0619a675af3bd7f5400ecf8ffd40c45
LAPTOP-L9O08U7Q\kalin <kaline-son@ya.ru>
2022-10-16 17:40:47 +0700

    add firering, collision and get out check

--- src/GaymPage.tsx
+++ src/GaymPage.tsx
@@ -1,119 +1,188 @@
 import React from "react";
-import { useKey } from "./useKey";
 
-interface ICanvasSettings {
+interface IGameObject {
+  id: string;
   width: number;
   height: number;
-  bgColor: string;
-  defaultUnit: {
-    canvasStyle: {
-      width: number;
-      height: number;
-    };
-    color: {
-      enemy: string;
-      ally: string;
-    };
-  };
-}
-interface IUnit {
-  type: "enemy" | "player";
-  canvasStyle: {
-    width: number;
-    height: number;
-  };
+  color: string;
   pos: {
     x: number;
     y: number;
   };
-  color: string;
-  delta: number;
+  delta?: number;
+  direction?: { x: 1 | -1 | 0; y: 1 | -1 | 0 };
+}
+interface ICanvasSettings extends IGameObject {}
+interface IBullet extends IGameObject {}
+interface IUnit extends IGameObject {
+  type: "enemy" | "player";
 }
 const canvasSettings: ICanvasSettings = {
-  width: 500,
-  height: 500,
-  bgColor: "green",
-  defaultUnit: {
-    canvasStyle: {
-      width: 40,
-      height: 40,
-    },
-    color: {
-      enemy: "red",
-      ally: "blue",
-    },
+  id: "canvas",
+  width: 600,
+  height: 300,
+  color: "green",
+  pos: {
+    x: 0,
+    y: 0,
   },
 };
 const myUnitDefault: IUnit = {
+  id: "player",
   type: "player",
-  canvasStyle: canvasSettings.defaultUnit.canvasStyle,
-  color: canvasSettings.defaultUnit.color.ally,
+  width: 40,
+  height: 40,
+  color: "blue",
   pos: {
-    x: 100,
+    x: 40,
+    y: 100,
+  },
+  delta: 15,
+  direction: { x: 0, y: 0 },
+};
+const enemyDefault: IUnit = {
+  id: "enemy",
+  type: "enemy",
+  width: 40,
+  height: 40,
+  color: "red",
+  pos: {
+    x: 250,
     y: 100,
   },
   delta: 5,
+  direction: { x: 0, y: 0 },
+};
+const bulletSettingsDefault: IBullet = {
+  id: "bullet",
+  height: 10,
+  width: 10,
+  color: "purple",
+  pos: {
+    x: 5,
+    y: 5,
+  },
+  delta: 1,
+  direction: { x: 1, y: 0 },
 };
 export function GaymPage() {
   const canvasRef = React.useRef<HTMLCanvasElement>(null);
-  const [myUnit, setMyUnit] = React.useState<IUnit>(myUnitDefault);
-  useKey("KeyW", () => moveUp(myUnit, setMyUnit));
-  useKey("KeyA", () => moveLeft(myUnit, setMyUnit));
-  useKey("KeyD", () => moveRight(myUnit, setMyUnit));
-  useKey("KeyS", () => moveDown(myUnit, setMyUnit));
+  const bullets: Array<IBullet> = [];
+  const myUnit = JSON.parse(JSON.stringify(myUnitDefault)) as IUnit;
+  const enemy = JSON.parse(JSON.stringify(enemyDefault)) as IUnit;
+  useKey("KeyW", () => move({ ...myUnit, direction: { x: 0, y: -1 } }));
+  useKey("KeyA", () => move({ ...myUnit, direction: { x: -1, y: 0 } }));
+  useKey("KeyD", () => move({ ...myUnit, direction: { x: 1, y: 0 } }));
+  useKey("KeyS", () => move({ ...myUnit, direction: { x: 0, y: 1 } }));
+  useKey("Space", () =>
+    createBullet(bullets, {
+      ...bulletSettingsDefault,
+      id: String(new Date().getTime()),
+      pos: { x: myUnit.pos.x + bulletSettingsDefault.width / 2 + myUnit.width / 2, y: myUnit.pos.y },
+    }),
+  );
   React.useEffect(() => {
     if (canvasRef.current) {
-      const ctx = canvasRef.current.getContext("2d")!;
-      drawBg(ctx, canvasSettings);
-      drawUnit(ctx, myUnit);
+      const canvas = canvasRef.current;
+      canvas.width = canvasSettings.width;
+      canvas.height = canvasSettings.height;
+      const ctx = canvas.getContext("2d")!;
+      drawGame(ctx, myUnit, enemy, bullets);
     }
-  });
+  }, []);
   return <canvas ref={canvasRef}></canvas>;
 }
 
-function drawBg(ctx: CanvasRenderingContext2D, settings: ICanvasSettings): void {
+function drawRect(ctx: CanvasRenderingContext2D, x: number, y: number, w: number, h: number, color: string) {
   ctx.beginPath();
-  ctx.rect(0, 0, settings.width, settings.height);
-  ctx.fillStyle = settings.bgColor;
+  ctx.rect(x, y, w, h);
+  ctx.fillStyle = color;
   ctx.fill();
   ctx.closePath();
 }
-function drawUnit(ctx: CanvasRenderingContext2D, settings: IUnit): void {
+function drawGame(ctx: CanvasRenderingContext2D, myUnit: IUnit, enemy: IUnit, bullets: Array<IBullet>) {
+  console.log(bullets);
+  drawBg(ctx, canvasSettings);
+  drawUnit(ctx, myUnit);
+  drawUnit(ctx, enemy);
+  for (let i = 0; i < bullets.length; i++) {
+    const bullet = bullets[i];
+    move(bullet);
+    if (checkCollision(bullet, enemy) || checkOutSide(ctx, bullet)) {
+      removeBullet(bullets, i);
+      i--;
+    }
+  }
+  drawBullets(ctx, bullets);
+  requestAnimationFrame(() => drawGame(ctx, myUnit, enemy, bullets));
+}
+function drawBg(ctx: CanvasRenderingContext2D, canvas: ICanvasSettings): void {
+  drawRect(ctx, 0, 0, canvas.width, canvas.height, canvas.color);
+}
+function drawUnit(ctx: CanvasRenderingContext2D, unit: IUnit): void {
   const pos = {
-    x: settings.pos.x - settings.canvasStyle.width / 2,
-    y: settings.pos.y - settings.canvasStyle.height / 2,
+    x: unit.pos.x - unit.width / 2,
+    y: unit.pos.y - unit.height / 2,
   };
-  ctx.beginPath();
-  ctx.rect(pos.x, pos.y, settings.canvasStyle.width, settings.canvasStyle.height);
-  ctx.fillStyle = settings.color;
-  ctx.fill();
-  ctx.closePath();
+  drawRect(ctx, pos.x, pos.y, unit.width, unit.height, unit.color);
 }
-function moveUp(unit: IUnit, setUnit: Function) {
-  const newValue = unit.pos.y - unit.delta;
-  setUnit((prev: IUnit) => ({
-    ...prev,
-    pos: { ...prev.pos, y: newValue },
-  }));
+function drawBullets(ctx: CanvasRenderingContext2D, bullets: Array<IBullet>) {
+  bullets.forEach((bullet: IBullet) => {
+    const pos = {
+      x: bullet.pos.x - bullet.width / 2,
+      y: bullet.pos.y - bullet.height / 2,
+    };
+    drawRect(ctx, pos.x, pos.y, bullet.width, bullet.height, bullet.color);
+  });
 }
-function moveLeft(unit: IUnit, setUnit: Function) {
-  const newValue = unit.pos.x - unit.delta;
-  setUnit((prev: IUnit) => ({
-    ...prev,
-    pos: { ...prev.pos, x: newValue },
-  }));
+function createBullet(bullets: Array<IBullet>, bullet: IBullet) {
+  bullets.push({ ...bullet });
 }
-function moveRight(unit: IUnit, setUnit: Function) {
-  const newValue = unit.pos.x + unit.delta;
-  setUnit((prev: IUnit) => ({
-    ...prev,
-    pos: { ...prev.pos, x: newValue },
-  }));
+function checkCollision(firstGO: IGameObject, secondGO: IGameObject): boolean {
+  let result = false;
+  const topCheck = firstGO.pos.y - firstGO.height / 2 <= secondGO.pos.y + secondGO.height / 2;
+  const bottomCheck = firstGO.pos.y + firstGO.height / 2 >= secondGO.pos.y - secondGO.height / 2;
+  const leftCheck = firstGO.pos.x - firstGO.width / 2 <= secondGO.pos.x + secondGO.width / 2;
+  const rightCheck = firstGO.pos.x + firstGO.width / 2 >= secondGO.pos.x - secondGO.width / 2;
+  if (secondGO.pos.x >= firstGO.pos.x) {
+    if (secondGO.pos.y >= firstGO.pos.y) {
+      result = rightCheck && bottomCheck;
+    } else if (secondGO.pos.y < firstGO.pos.y) {
+      result = rightCheck && topCheck;
+    }
+  } else if (secondGO.pos.x < firstGO.pos.x) {
+    if (secondGO.pos.y >= firstGO.pos.y) {
+      result = leftCheck && bottomCheck;
+    } else if (secondGO.pos.y < firstGO.pos.y) {
+      result = leftCheck && topCheck;
+    }
+  }
+  return result;
 }
-function moveDown(unit: IUnit, setUnit: Function) {
-  const newValue = unit.pos.y + unit.delta;
-  setUnit((prev: IUnit) => ({
-    ...prev,
-    pos: { ...prev.pos, y: newValue },
-  }));
+function checkOutSide(ctx: CanvasRenderingContext2D, gO: IGameObject): boolean {
+  const xOutSide = gO.pos.x - gO.width / 2 >= ctx.canvas.width || gO.pos.x + gO.width / 2 <= 0;
+  const yOutSide = gO.pos.y - gO.height / 2 >= ctx.canvas.height || gO.pos.y + gO.height / 2 <= 0;
+  return xOutSide || yOutSide;
+}
+//splice! отсеиваю не нужные пули по индексу!
+function removeBullet(bullets: Array<IBullet>, index: number) {
+  bullets.splice(index, 1);
+}
+const useKey = (key: string, callback: () => void, canUse = true) => {
+  React.useEffect(() => {
+    if (canUse) {
+      const handlePress = (e: KeyboardEvent) => {
+        if (e.code === key) {
+          e.preventDefault();
+          callback();
+        }
+      };
+      document.addEventListener("keydown", handlePress);
+      return () => document.removeEventListener("keydown", handlePress);
+    }
+  });
+};
+function move(obj: IGameObject) {
+  obj.pos.x += obj.delta! * obj.direction!.x;
+  obj.pos.y += obj.delta! * obj.direction!.y;
 }
